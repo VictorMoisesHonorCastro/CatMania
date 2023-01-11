fd0f23d996d27370ee5f90d3a0cb1a88a7fe40f6
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-01-11 19:09:46 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-const routes: Routes = [];
+//TODO: Definir las ruta/s que correspondan
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: '', loadChildren:()=>import('./pages/pages.module').then((m)=>m.PagesModule),canActivate:[AuthGuard]},
+  {path: '**', redirectTo: '/login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
