4d537396d8ba03ae689cd156cff7a7f9c63766b4
VictorMoisesHonorCastro <73166294+VictorMoisesHonorCastro@users.noreply.github.com>
2023-01-16 19:55:22 +0100

    catmania2

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 //TODO: Definir las ruta/s que correspondan
 const routes: Routes = [
+  { path: 'main', component:MainPageComponent},
+  { path: 'login', component:LoginPageComponent}
 ];
 
 @NgModule({
